e78fbf840b10b7cf872db87a861d15c30a2d5e13
miyamae <miya.y.1201@gmail.com>
2019-06-26 14:22:51 +0900

    git init

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (_req: Request, res: Response) => res.send('Hello World!!'))
+
+app.listen(3000, () => console.log('listening on port 3000'))
